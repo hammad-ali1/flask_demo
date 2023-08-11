f085601218c820a8b605f6e334bdefde498cd7fc
Hammad Ali <hammad.a22002@gmail.com>
2023-08-11 23:52:05 +0500

    add dotenv variables

--- api/app.py
+++ api/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+# Load config
+if os.environ.get('MODE') == 'DEVELOPMENT':
+    app.config.from_object('config.config_development')
+elif os.environ.get('MODE') == 'PRODUCTION':
+    app.config.from_object('config.config_production')
 
 
 @app.route("/")
